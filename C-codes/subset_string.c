c24f4c5e2d6b5fd166de1f0d5a7f6de9529b076d
Salma Shihab <115235631+SalmaShihabOfficial@users.noreply.github.com>
2023-08-05 13:09:51 +0530

    Update subset_string.c

--- C-codes/subset_string.c
+++ C-codes/subset_string.c
@@ -4,9 +4,11 @@
 void substring(char[], char[], int , int );
 	void main()
 	{
-	    char c[10],str[10] ="ABC";
-	    int i, j, len = strlen(str);
-
+	    char c[10],str[10];
+	    int i, j, len;
+        printf("Enter the string:");
+        scanf("%s",str);
+        len = strlen(str);
 	    printf("All subsets for the given string are:  ");
 	    //This loop maintains the starting character
 	    for(i = 0; i < len; i++){
@@ -27,3 +29,29 @@ void substring(char[], char[], int , int );
 		   }
 		   sub[c] = '\0';
 		}
+
+
+/*
+Enter the string:raehho
+All subsets for the given string are:  r
+ra
+rae
+raeh
+raehh
+raehho
+a
+ae
+aeh
+aehh
+aehho
+e
+eh
+ehh
+ehho
+h
+hh
+hho
+h
+ho
+o
+*/
